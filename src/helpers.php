f5ff35828f76e7dd35c040b14875091c2bfd5065
Mikhail Panyushkin <1970673+mpanius@users.noreply.github.com>
2021-11-27 22:05:16 +0300

    add check webp - if no http responce

--- src/helpers.php
+++ src/helpers.php
@@ -2,6 +2,6 @@
 
 if (!function_exists('supportsWebp')) {
     function supportsWebp() {
-        return strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false;
+        return strpos($_SERVER['HTTP_ACCEPT'] ?? '', 'image/webp') !== false;
     }
 }
